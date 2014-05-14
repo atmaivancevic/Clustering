c21519a6a75298c8779be3c96ae8196931e36779
Atma <atma.ivancevic@adelaide.edu.au>
2014-05-14 17:38:59 +0930

    just need to write if statement

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // Example Usage: Clustering animal, e.g. Clustering Aardvark
@@ -11,24 +12,35 @@ import (
 func main() {
 
 	animal := os.Args[1]
-	id := os.Args[2]
 
-	inputFileName := animal + "_" + id + "_sorted"
+	for i := 70; i < 100; i += 5 {
 
-	// open inputFile in read-only mode
-	inputFile, inputError := os.Open(inputFileName)
-	if inputError != nil {
-		fmt.Println("An error occurred opening the input file.")
-		return
-	}
-	defer inputFile.Close()
+		//define the input file
+		inputFileName := animal + "_0." + strconv.Itoa(i) + "_sorted"
 
-	inputScanner := bufio.NewScanner(inputFile)
-	for inputScanner.Scan() {
-		fmt.Println(inputScanner.Text())
+		// open input file in read-only mode
+		inputFile, inputError := os.Open(inputFileName)
+		if inputError != nil {
+			fmt.Println("An error occurred opening the input file.")
+			return
+		}
+		defer inputFile.Close()
+
+		// read the input file
+		inputScanner := bufio.NewScanner(inputFile)
+		for inputScanner.Scan() {
+		}
+		if err := inputScanner.Err(); err != nil {
+			fmt.Println("An error occurred while reading the file.")
+			return
+		}
+
+		if  {
+			break
+		}
+		fmt.Println("Best clustering % identity:", i)
 	}
-	if err := inputScanner.Err(); err != nil {
-		fmt.Println("An error occurred while reading the file.")
-		return
-	}
+
 }
+
+// fmt.Println(inputScanner.Text(), i)
