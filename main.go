3afa176af55b8650f1bc64833be6ad707623de74
Atma <atma.ivancevic@adelaide.edu.au>
2014-05-15 16:46:09 +0930

    finished! hopefully. works on Aardvark.

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // Example Usage: Clustering animal, e.g. Clustering Aardvark
-// id = the unique clustering % id of each input file, ranging from 0.70 to 0.95 in 0.05 increments
+// i = the unique clustering % id of each input file, ranging from 0.70 to 0.95 in 0.05 increments
 func main() {
 
 	animal := os.Args[1]
 
+OuterLoop:
 	for i := 70; i < 100; i += 5 {
-
 		//define the input file
 		inputFileName := animal + "_0." + strconv.Itoa(i) + "_sorted"
+		var r *bufio.Reader
 
 		// open input file in read-only mode
 		inputFile, inputError := os.Open(inputFileName)
@@ -26,21 +28,36 @@ func main() {
 		}
 		defer inputFile.Close()
 
-		// read the input file
-		inputScanner := bufio.NewScanner(inputFile)
-		for inputScanner.Scan() {
-		}
-		if err := inputScanner.Err(); err != nil {
-			fmt.Println("An error occurred while reading the file.")
-			return
-		}
+		r = bufio.NewReader(inputFile)
 
-		if  {
-			break
+		for a := 0; a < 1; a++ {
+			mystring, err := r.ReadString('\t')
+
+			// fmt.Println(mystring)
+			mystring = strings.TrimSuffix(mystring, "\t")
+
+			x, _ := strconv.Atoi(mystring)
+			// 	id, _ := strconv.Atoi(i)
+
+			fmt.Println("Clustering % id:", i)
+			fmt.Println("Max size of any cluster:", x)
+
+			if x <= 100 ||
+				(i == 90 && x <= 500) ||
+				(i > 90) {
+				fmt.Println("This is the best clustering % id:", i)
+				break OuterLoop
+			} else {
+				fmt.Println("Not alignable, too many seqs per cluster!")
+				continue
+			}
+
+			if err != nil {
+				fmt.Println(err)
+				return
+
+			}
 		}
-		fmt.Println("Best clustering % identity:", i)
 	}
 
 }
-
-// fmt.Println(inputScanner.Text(), i)
